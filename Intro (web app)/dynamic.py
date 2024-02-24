35ee869794aeb0cb35d9054875846f5b7c7831d0
Nitya Verma <vrmnitya@gmail.com>
2024-02-24 12:12:05 +0530

    build URL dynamically

---
+++ Intro (web app)/dynamic.py
@@ -0,0 +1,36 @@
+#Building URL dynamically
+
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return "Welcome"
+
+
+@app.route('/success/<int:score>')   #score is an integer value
+def success(score):
+    return "The person has passed and the marks are"  + str(score)
+
+
+@app.route('/fail/<int:score>')   #score is an integer value
+def fail(score):
+    return "The person has failed and the marks are"  + str(score)
+
+
+#===result checker===#
+@app.route('/results/<int:marks>')
+def results(marks):
+    result = ""
+    if marks<50:
+        result = "fail"
+    else:
+        result= "success"
+
+    return redirect(url_for(result,score= marks))
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
